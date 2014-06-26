da0b021ccfeda945a648f2e05a60440601f118d9
Robert Rothenberg <rrwo@cpan.org>
2014-06-26 22:23:04 +0100

    Added POD, code tweaks

--- lib/Plack/Middleware/BlockHeaderInjection.pm
+++ lib/Plack/Middleware/BlockHeaderInjection.pm
@@ -10,6 +10,37 @@ use Plack::Util::Accessor qw( logger status );
 
 use version 0.77; our $VERSION = version->declare('v0.1.0');
 
+=head1 NAME
+
+Plack::Middleware::BlockHeaderInjection - block header injections in responses
+
+=head1 SYNOPSIS
+
+  use Plack::Builder;
+
+  my $app = ...
+
+  $app = builder {
+    enable 'BlockHeaderInjection',
+      status => 500;
+    $app;
+  };
+
+=head1 DESCRIPTION
+
+This middleware will check responses for injected headers. If the
+headers contain newlines, then the return code is set to C<500> and
+the offending header(s) are removed.
+
+=head1 OPTIONS
+
+=head2 C<status>
+
+The status code to return if an invalid header is found. By default,
+this is C<500>.
+
+=cut
+
 sub call {
     my ( $self, $env ) = @_;
 
@@ -28,16 +59,19 @@ sub call {
 
             # Sanity check headers
 
-            my @headers = @{$res->[1]};
+            my $hdrs = $res->[1];
 
-            while (my $key = shift @headers) {
-                my $val = shift @headers;
+            my $i = 0;
+            while ($i < @{$hdrs}) {
+                my $val = $hdrs->[$i+1];
                 if ($val =~ /[\n\r]/) {
-                    $self->log( error => 'possible header injection detected' );
+                    my $key = $hdrs->[$i];
+                    $self->log(
+                        error => "possible header injection detected in ${key}" );
                     $res->[0] = $self->status;
-                    Plack::Util::header_remove($res->[1], $key);
-                    return $res;
+                    Plack::Util::header_remove($hdrs, $key);
                 }
+                $i+=2;
             }
 
         }
@@ -51,10 +85,58 @@ sub log {
     my ($self, $level, $msg) = @_;
     $self->logger->({
         level   => $level,
-        message => "Security::Simple: ${msg}",
+        message => "BlockHeaderInjection: ${msg}",
     });
 }
 
+=head1 AUTHOR
 
+Robert Rothenberg, C<< <rrwo at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Robert Rothenberg.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+=for readme stop
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=for readme continue
+
+=cut
 
 1;
