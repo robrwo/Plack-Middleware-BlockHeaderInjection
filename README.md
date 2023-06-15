4b9f01171094ba6b095bd6c7acc2b6ae05de013e
Robert Rothenberg <rrwo@cpan.org>
2023-06-15 23:39:06 +0100

    Bump VERSION due to minimum Perl change

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Plack::Middleware::BlockHeaderInjection - block header injections in responses
 
 # VERSION
 
-version v1.0.2
+version v1.1.0
 
 # SYNOPSIS
 
--- lib/Plack/Middleware/BlockHeaderInjection.pm
+++ lib/Plack/Middleware/BlockHeaderInjection.pm
@@ -10,7 +10,7 @@ use parent qw( Plack::Middleware );
 use Plack::Util;
 use Plack::Util::Accessor qw( logger status );
 
-our $VERSION = 'v1.0.2';
+our $VERSION = 'v1.1.0';
 
 =head1 SYNOPSIS
 
